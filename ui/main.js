d1a07a0f6eac24769083af129d87133b44236dcd
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-03-26 20:32:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //to change the content of the webpage
 var element= document.getElementById('text');
-element.innerHTML="bthgrfreh";
+element.innerHTML="welcome";
 
 //to shift the image
 /*var img=document.getElementById("mad");
