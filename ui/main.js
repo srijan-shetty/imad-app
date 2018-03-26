977cf339dbec27e6d3e582860d58d10e070e8b76
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-03-26 19:45:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,10 +14,10 @@ img.onclick = function () {
 var img = document.getElementById("mad");
 var marginLeft=0;
 function moveRight () {
-    marginLeft=marginLeft + 10;
+    marginLeft=marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
     
 };
