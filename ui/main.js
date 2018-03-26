beb244d8ca0a68649bf500d4dca212e82e0513d5
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-03-26 17:40:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element= document.getElementById('text');
 element.innerHTML="bthgrfreh";
 
 //to shift the image
-var img=document.getElementById("mad");
+var img=document.getElementById("#mad");
 img.onclick = function () {
     img.style.marginleft="100px";
 };
