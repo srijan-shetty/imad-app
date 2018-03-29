0b2f5c3fa2187be8693159448945f071dab8d6b0
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-03-29 15:42:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ img.onclick = function () {
 };*/
 //to shift image by using function call
 
-var img = document.getElementById("mad");
+/*var img = document.getElementById("mad");
 var marginLeft=0;
 function moveRight () {
     marginLeft=marginLeft + 1;
@@ -20,4 +20,11 @@ function moveRight () {
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
     
+};*/
+var counter=0;
+var button=document.getElementById("counter");
+button.onclick=function() {
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
 };
