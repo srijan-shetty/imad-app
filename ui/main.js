a114c4b1c45b658f17854f7184f01a57304cfd0f
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-03-29 15:58:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,8 +32,8 @@ button.onclick=function() {
             //take some action
             if(request.status===200) {
                 var counter=request.responseText;
-                 var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
             }
         }
        //not dne yet 
