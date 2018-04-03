380c61406ba42e0b651a9786adb4177b22765780
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-04-03 18:47:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var config={
+    user:'srijanssnl406',
+    database:'srijanssnl406',
+    host:'http://db.imad.hasura-app.io',
+    port:'5342',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,15 +16,24 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var Pool= new Pool(config);
 
-app.get('/test_db',function(req,res) {
+app.get('/test-db',function(req,res) {
    //make a request
    //response
+   Pool.query('SELECT * FROM test', function(err,result) {
+       if(err) {
+       res.status(500).send(err.toString);
+       }else {
+           res.send(JSON.stringify(result));
+       }
+       
+   });
 });
-var counter=0
+var counter=0;
 app.get('/counter',function(req,res) {
     counter=counter+1;
-    res.send(counter.toString())
+    res.send(counter.toString());
 });
 app.get('/article-one', function (req,res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
