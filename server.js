737248b388cbd97809feb0d691351efa7bf04846
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-04-03 19:04:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/test-db',function(req,res)
 {
 	//make a query
 		//return a response
-	Pool.query('select * from test',function(err,result) 
+	pool.query('select * from test',function(err,result) 
     { 
 		if(err)
         {
@@ -77,7 +77,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 5432;
+var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
