ef3e2bfd18d4541bae261f7860ab7fd7b8ae038b
srijan-shetty <36466814+srijan-shetty@users.noreply.github.com>
2018-04-10 22:09:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.get('/', function (req, result) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+function hash(input,salt){
+    var hashed=crypto.pbkdf25sync(input,salt,1000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req,result){
+   var hashedString=hash(req.params.input, 'this-is-some-random-arguments');
+   res.send(hashedString);
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
